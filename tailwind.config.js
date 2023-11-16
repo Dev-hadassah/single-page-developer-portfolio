15bc47e7b02c604776ffa0fde0e068b2de90b3f1
Hadassah <faridathadassah4@gmail.com>
2023-11-16 11:19:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#151515',
+        lightGreen: '#4EE1A0',
+        white: '#ffffff',
+        darkGrey: '#242424',
+        grey: '#d9d9d9'
+
+      },
+      fontFamily: {
+        SpaceGrotesk: ['SpaceGrotesk', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
+
+
